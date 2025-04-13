243f2167e0b5ccdc782a040c13f023b2baba560d
athulganesh7 <athulganesh777@gmail.com>
2025-04-14 01:04:17 +0530

    landing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 import Landing from './pages/Landing'
+import Home from './pages/Home'
+import Wishlist from './pages/Wishlist'
+import Visited from './pages/Visited'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Landing/>
-     
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/visited" element={<Visited />} />
+      </Routes>
     </>
   )
 }
